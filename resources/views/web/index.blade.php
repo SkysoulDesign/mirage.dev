d7dd057384ebbd07a97a281267218819dd6ca6c0
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-03-18 14:07:21 +0800

    user mobile app - web blade

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vivek
+ * Date: 3/18/16
+ * Time: 2:04 PM
+ */
+@extends('layouts.master')
+
+@section('content')
+
+    {{ dd($code) }}
+
+@endsection
