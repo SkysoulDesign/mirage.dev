0071b4e013bfa6433a293e94d29d0cd1f30da82b
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-03-18 19:23:40 +0800

    Create section for users to login via website and see the codes as like as mobile app

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -9,7 +9,8 @@
             <div class="column">
                 <div class="ui fluid card">
                     <a href="{{ route('web.product.view', $code->id)  }}" class="image">
-                        <img src="{{ $code->product->profile->image or $code->product->image }}">
+                        <img src="{{ $code->product->image }}">
+{{--                        {{ $code->product->profile->image or }}--}}
                     </a>
                     <div class="content">
                         <a class="header">{{ $code->product->name }}</a>
