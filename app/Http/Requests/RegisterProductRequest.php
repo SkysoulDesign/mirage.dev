dcdb007285aafaa4314eccc29cc64b9db6c939f4
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-03-18 19:11:01 +0800

    update

---
+++ app/Http/Requests/RegisterProductRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+class RegisterProductRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user() ?: true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'code' => 'required|exists:codes'
+        ];
+    }
+}
---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+/**
+ * Class RegisterUserRequest
+ * @package App\Http\Requests
+ */
+class RegisterUserRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required|alpha_dash',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+            'gender' => 'string',
+            'age_id' => 'exists:ages,id',
+            'terms' => 'required|accepted',
+            'country_id' => 'required|exists:countries,id',
+        ];
+    }
+}
