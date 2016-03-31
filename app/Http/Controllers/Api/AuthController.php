32b81a54c12813da1222f1d741bcca659a17389f
Vivekanandan Krishnan <vivek@skysoul.com.au>
2016-03-31 15:59:42 +0800

    inject Product Combination

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\InjectProductTrait;
 use App\Jobs\Users\CheckTokenJob;
 use App\Jobs\Users\CreateUserJob;
 use App\Jobs\Users\GenerateTokenJob;
@@ -12,7 +13,6 @@ use App\Models\Product;
 use App\Models\User;
 use Illuminate\Contracts\Auth\Factory as Auth;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 
 /**
  * Class AuthController
@@ -21,6 +21,9 @@ use Illuminate\Support\Collection;
  */
 class AuthController extends Controller
 {
+
+    use InjectProductTrait;
+
     /**
      * @var Auth
      */
@@ -185,60 +188,6 @@ class AuthController extends Controller
 
     }
 
-    /**
-     * injectProductCombo:
-     * if it's an user, check product combination (1, 2, 3) exists and auto inject product (12)
-     * also need to do in reverse
-     * @param User $user
-     */
-    protected function injectProductCombo(User $user)
-    {
-        if (!$user->is('admin')) {
-            $codes = $user->getRelation('codes');
 
-            if($codes->isEmpty()) return;
-
-            $productCombo = collect([1, 2, 3]);
-            $injectProduct = collect([12]);
-
-
-            /** @var Collection $prdIdArr */
-            $prdIdArr = $codes->pluck('product_id')->unique()->toArray();
-
-
-            $diff = $productCombo->diff($prdIdArr);
-            $diffInverse = $injectProduct->diff($prdIdArr);
-
-            $product = collect();
-
-            if ($diff->isEmpty()) {
-                $product = $diffInverse;
-            } else if ($diffInverse->isEmpty()) {
-                $product = $diff;
-            }
-
-            if (!$product->isEmpty()) {
-                $code = $this->getCodeByProduct($product->toArray());
-                if ($code) {
-                    $codes = $codes->merge($code);
-                }
-
-                $user->setRelation('codes', $codes);
-            }
-        }
-    }
-
-    /**
-     * @param $product
-     * @return $this
-     */
-    protected function getCodeByProduct($product)
-    {
-        $code = Product::all()->whereIn('id', $product)->transform(function ($product) {
-            /** @var Code $code */
-            return $product->codes()->with('product', 'product.extras', 'product.profile')->first();
-        });
-        return $code;
-    }
 
 }
